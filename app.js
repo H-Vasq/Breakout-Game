7b968429d59ea701e6a31b1b8ab7bd4ec98138b5
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-06-16 15:33:42 +0000

    create Ball object

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 class Ball {
     constructor(x, y, radius, speedX, speedY) {
-        this
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+    draw(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fillStyle = "purple";
+        context.fill();
+        context.closePath();
+    }
+    update() {
+        this.X += this.speedX;
+        this.y += this.speedY;
     }
 }
