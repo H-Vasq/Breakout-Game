9c2e054191c050c5b8fc81b0d0fa527e4bd8e217
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-01 17:06:07 +0000

    start brick class

--- app.js
+++ app.js
@@ -36,6 +36,21 @@ class Paddle {
     }
 }
 
+class Brick {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    draw(context) {
+        context.fillStyle = "red";
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}   
+
+
+
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d")
 
