21aafd6b023ff4716603f9c70a85e4de6b15cf8b
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-04 20:08:26 +0000

    continue

--- factory.js
+++ factory.js
@@ -1,1 +1,13 @@
-function createPerson(name)
+function createPerson(name) {
+    return {
+        name: name,
+        talk() {
+            return `I am ${this.name}`
+        }
+    }
+}
+
+const me = createPerson('Bob')
+const you = createPerson('Harry')
+
+console.log(me)
