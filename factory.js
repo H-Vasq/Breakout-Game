0a2bb5435126593da2bc77f196ff7063038fd2d0
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-04 20:36:14 +0000

    know what is code duplicaiton

--- factory.js
+++ factory.js
@@ -1,13 +1,7 @@
-function Person(name) {
-    this.name = name
+const me = {
+    talk() {
+        return 'Talking';
+    }
 }
 
-const doggy = new Person('Doggy')
-
-Person.prototype.talk = function() {
-    return `Ciao, io sono ${this.name}`
-}
-
-const mary = new Person('Mary');
-
-console.log(mary.talk())
+me.talk();
